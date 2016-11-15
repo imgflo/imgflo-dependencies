d7ea67cb6672f7a6032b408172d0f056faf60cca
Jon Nordby <jononor@gmail.com>
2016-11-15 21:59:52 +0100

    travis: Install nasm, needed by ffmpeg

--- travis/install-dependencies.sh
+++ travis/install-dependencies.sh
@@ -7,4 +7,5 @@ else
     sudo apt-get --assume-yes build-dep gegl
     sudo apt-get --assume-yes install libjson-glib-dev libsdl1.2-dev
     sudo apt-get --assume-yes install sqlite3
+    sudo apt-get --assume-yes install nasm # for ffmpeg
 fi
